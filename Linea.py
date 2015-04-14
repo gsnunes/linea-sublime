23cde197279984ec58a358eed6f9f37873724cc1
Guilherme Nunes <guinunesilva@gmail.com>
2015-04-14 17:07:43 -0300

    improve to create another templates

--- Linea.py
+++ Linea.py
@@ -10,10 +10,10 @@ class lineaCommand(sublime_plugin.WindowCommand):
     def run(self, dirs, type):
     	self.window.show_input_panel("Widget name:", "", functools.partial(self.on_done, dirs[0], type), None, None)
 
-    def on_done(self, dir, name, type):
-		p = subprocess.Popen('linea generate widget "' + type + ' ' + dir + "\\" + name + '"', stdout=subprocess.PIPE, shell=True)
+    def on_done(self, dir, type, name):
+		p = subprocess.Popen('linea generate ' + type + ' "' + dir + "\\" + name + '"', stdout=subprocess.PIPE, shell=True)
 		(output, err) = p.communicate()
 		print output
 
-    def is_enabled(self, dirs):
-        return len(dirs) > 0
+    def is_enabled(self, dirs, type):
+    	return len(dirs) > 0
